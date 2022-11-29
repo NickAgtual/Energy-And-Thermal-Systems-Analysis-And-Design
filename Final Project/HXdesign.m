67b371225350a86ff81a884ef9c8390a7d0597c9
Nick Agtual <nick.agtual@icloud.com>
2022-11-28 22:28:42 -0800

    Added plotting feature to show geometry of HX designs

--- Final Project/HXdesign.m
+++ Final Project/HXdesign.m
@@ -396,7 +396,7 @@ verificationWater.outletTemp = (verificationAir.C / ...
 
 %% Plots
 
-% Pressure drops across different HX
+% ----- Pressure drops across different HX -----
 % Creating new figure
 figure(1)
 
@@ -423,7 +423,75 @@ ylabel('\emph {Pressure Drop (Pa)}','fontsize',14,'Interpreter','latex');
 title('\emph {Pressure Drop Across Different HX}','fontsize',16,...
     'Interpreter', 'latex')
 
+% ----- Geometry of HX -----
+% Creating new Figure
+figure(2)
 
+% RGB color vectors
+rgbColors = {[0 204 0] / 255, [0 102 204] / 255, [255 0 0] / 255};
 
+% Plotting HX size for each type
+for ii = 1:3
+    plotcube([HX.width(ii) HX.height(ii) HX.Ltm(ii)], [0 0 0],...
+        .9, rgbColors{ii})
+    hold on
+end
 
-
+% Plot descriptors
+xlabel('\emph {Heat Exchanger Width}','fontsize',14,'Interpreter',...
+    'latex');
+ylabel('\emph {Heat Exchanger Lenght}','fontsize',14,'Interpreter',...
+    'latex');
+zlabel('\emph {Heat Exchanger Height}','fontsize',14,'Interpreter',...
+    'latex');
+title('\emph {Heat Exchanger Geometry}','fontsize',16,...
+    'Interpreter', 'latex')
+%% Cube Plotting Function
+function plotcube(varargin)
+% PLOTCUBE - Display a 3D-cube in the current axes
+%
+%   PLOTCUBE(EDGES,ORIGIN,ALPHA,COLOR) displays a 3D-cube in the current axes
+%   with the following properties:
+%   * EDGES : 3-elements vector that defines the length of cube edges
+%   * ORIGIN: 3-elements vector that defines the start point of the cube
+%   * ALPHA : scalar that defines the transparency of the cube faces (from 0
+%             to 1)
+%   * COLOR : 3-elements vector that defines the faces color of the cube
+%
+% Example:
+%   >> plotcube([5 5 5],[ 2  2  2],.8,[1 0 0]);
+%   >> plotcube([5 5 5],[10 10 10],.8,[0 1 0]);
+%   >> plotcube([5 5 5],[20 20 20],.8,[0 0 1]);
+% Default input arguments
+inArgs = { ...
+  [10 56 100] , ... % Default edge sizes (x,y and z)
+  [10 10  10] , ... % Default coordinates of the origin point of the cube
+  .7          , ... % Default alpha value for the cube's faces
+  [1 0 0]       ... % Default Color for the cube
+  };
+% Replace default input arguments by input values
+inArgs(1:nargin) = varargin;
+% Create all variables
+[edges,origin,alpha,clr] = deal(inArgs{:});
+XYZ = { ...
+  [0 0 0 0]  [0 0 1 1]  [0 1 1 0] ; ...
+  [1 1 1 1]  [0 0 1 1]  [0 1 1 0] ; ...
+  [0 1 1 0]  [0 0 0 0]  [0 0 1 1] ; ...
+  [0 1 1 0]  [1 1 1 1]  [0 0 1 1] ; ...
+  [0 1 1 0]  [0 0 1 1]  [0 0 0 0] ; ...
+  [0 1 1 0]  [0 0 1 1]  [1 1 1 1]   ...
+  };
+XYZ = mat2cell(...
+  cellfun( @(x,y,z) x*y+z , ...
+    XYZ , ...
+    repmat(mat2cell(edges,1,[1 1 1]),6,1) , ...
+    repmat(mat2cell(origin,1,[1 1 1]),6,1) , ...
+    'UniformOutput',false), ...
+  6,[1 1 1]);
+cellfun(@patch,XYZ{1},XYZ{2},XYZ{3},...
+  repmat({clr},6,1),...
+  repmat({'FaceAlpha'},6,1),...
+  repmat({alpha},6,1)...
+  );
+view(3);
+end
